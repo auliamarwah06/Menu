be433b544d52b4157740cf4edb6e944ed735e107
auliamarwah06 <marwahaulia724@gmail.com>
2025-01-08 21:27:00 +0700

    Add files via upload

---
+++ Menu.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tubesss;
+
+/**
+ *
+ * @author LENOVO
+ */
+abstract class Menu {
+     protected String nama;
+    protected int harga;
+
+    public Menu(String nama, int harga) {
+        this.nama = nama;
+        this.harga = harga;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public int getHarga() {
+        return harga;
+    }
+
+    public abstract void displayMenu();
+}
